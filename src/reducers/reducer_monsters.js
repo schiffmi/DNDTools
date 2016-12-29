dd90999e07bbfcb171cf2734b5affe7927958530
Michael Schiff <schiff.mi@gmail.com>
2016-12-29 16:18:52 -0500

    reselect

--- src/reducers/reducer_monsters.js
+++ src/reducers/reducer_monsters.js
@@ -1,4 +1,5 @@
 import { GET_MONSTERS, SORT_MONSTERS, FILTER_MONSTERS } from '../actions/types';
+import { createSelector } from 'reselect'
 
 const initialState = {
     all: {},
@@ -23,35 +24,82 @@ export default function(state = initialState, action) {
   }
 }
 
-export const getSortedMonsters = (state) => {
-  const item = id => state.all[id];
-  let sortString = state.sort;
-  let asc = state.ascending;
-  let sortedList;
-  switch (state.sort){
-    case 'name':
-      sortedList = [...state.lists[state.selectedList]].sort( (a, b) => {
-        return asc ? item(a)[sortString].localeCompare(item(b)[sortString]) : item(b)[sortString].localeCompare(item(a)[sortString])
-      });
-      break;
-    case 'challenge_rating':
-      let challenge = cr => {
-        let split = cr.split('/');
-        if (split.length === 1) return parseFloat(split[0].trim());
-        else {
-          return parseFloat(split[0].trim()) / parseFloat(split[1].trim());
+
+const getAllMonsters = (state) => state.all;
+const getAllLists = (state) => state.lists;
+const getSortString = (state) => state.sort;
+const getOrder = (state) => state.ascending;
+const getSelectedList = (state) => state.selectedList;
+const getFilter = (state) => state.filter;
+
+export const getSortedMonsters = createSelector(
+  [ getAllMonsters, getAllLists, getSortString, getOrder, getSelectedList, getFilter ],
+  ( allMonsters, lists, sort, ascending, selectedList, filter) => {
+    const item = id => allMonsters[id];
+
+    let sortString = sort;
+    let asc = ascending;
+    let sortedList;
+    switch (sortString){
+      case 'name':
+        sortedList = [...lists[selectedList]].sort( (a, b) => {
+          return asc ? item(a)[sortString].localeCompare(item(b)[sortString]) : item(b)[sortString].localeCompare(item(a)[sortString])
+        });
+        break;
+      case 'challenge_rating':
+        let challenge = cr => {
+          let split = cr.split('/');
+          if (split.length === 1) return parseFloat(split[0].trim());
+          else {
+            return parseFloat(split[0].trim()) / parseFloat(split[1].trim());
+          }
         }
-      }
-      sortedList = [...state.lists[state.selectedList]].sort( (a, b) => {
-          return asc ? challenge(item(a)[sortString]) - challenge(item(b)[sortString]) : challenge(item(b)[sortString]) - challenge(item(a)[sortString])
-        });
-      break;
-    default:
-      sortedList = [...state.lists[state.selectedList]].sort( (a,b) => {
-        return asc ? 
-          (state.all[a])[sortString] - (state.all[b])[sortString] : (state.all[b])[sortString] - (state.all[a])[sortString];
-        });
-      break;
+        sortedList = [...lists[selectedList]].sort( (a, b) => {
+            return asc ? challenge(item(a)[sortString]) - challenge(item(b)[sortString]) : challenge(item(b)[sortString]) - challenge(item(a)[sortString])
+          });
+        break;
+      default:
+        sortedList = [...lists[selectedList]].sort( (a,b) => {
+          return asc ? 
+            (allMonsters[a])[sortString] - (allMonsters[b])[sortString] : (allMonsters[b])[sortString] - (allMonsters[a])[sortString];
+          });
+        break;
+    }
+    return sortedList.map(id => allMonsters[id]).filter(monster => monster.name.toLowerCase().includes(filter));
   }
-  return sortedList.map(id => state.all[id]).filter(monster => monster.name.toLowerCase().includes(state.filter));     
-}
+)
+
+
+// export const getSortedMonsters = (state) => {
+//   const item = id => state.all[id];
+
+//   let sortString = state.sort;
+//   let asc = state.ascending;
+//   let sortedList;
+//   switch (state.sort){
+//     case 'name':
+//       sortedList = [...state.lists[state.selectedList]].sort( (a, b) => {
+//         return asc ? item(a)[sortString].localeCompare(item(b)[sortString]) : item(b)[sortString].localeCompare(item(a)[sortString])
+//       });
+//       break;
+//     case 'challenge_rating':
+//       let challenge = cr => {
+//         let split = cr.split('/');
+//         if (split.length === 1) return parseFloat(split[0].trim());
+//         else {
+//           return parseFloat(split[0].trim()) / parseFloat(split[1].trim());
+//         }
+//       }
+//       sortedList = [...state.lists[state.selectedList]].sort( (a, b) => {
+//           return asc ? challenge(item(a)[sortString]) - challenge(item(b)[sortString]) : challenge(item(b)[sortString]) - challenge(item(a)[sortString])
+//         });
+//       break;
+//     default:
+//       sortedList = [...state.lists[state.selectedList]].sort( (a,b) => {
+//         return asc ? 
+//           (state.all[a])[sortString] - (state.all[b])[sortString] : (state.all[b])[sortString] - (state.all[a])[sortString];
+//         });
+//       break;
+//   }
+//   return sortedList.map(id => state.all[id]).filter(monster => monster.name.toLowerCase().includes(state.filter));     
+// }
--- src/reducers/reducer_spells.js
+++ src/reducers/reducer_spells.js
@@ -1,4 +1,5 @@
 import { GET_SPELLS, SORT_SPELLS, FILTER_SPELLS  } from '../actions/types';
+import { createSelector } from 'reselect';
 
 const initialState = {
     all: {},
@@ -23,24 +24,56 @@ export default function(state = initialState, action) {
   }
 }
 
-export const getSortedSpells = (state) => {
-  const item = id => state.all[id];
-  let sortString = state.sort;
-  let asc = state.ascending;
-  let sortedList;
-  switch (state.sort){
+
+const allSpells = (state) => state.all;
+const sortString = (state) => state.sort;
+const ascending = (state) => state.ascending;
+const lists = (state) => state.lists;
+const selectedList = (state) => state.selectedList;
+const filter= (state) => state.filter;
+
+export const getSortedSpells = createSelector(
+  [allSpells, sortString, ascending, lists, selectedList, filter], 
+  (allSpells, sortString, ascending, lists, selectedList, filter) => {
+    const item = id => allSpells[id];
+    
+    let sortedList;
+    switch (sortString){
       //    case 'name':
       default:
-      sortedList = [...state.lists[state.selectedList]].sort( (a, b) => {
-        return asc ? item(a)[sortString].localeCompare(item(b)[sortString]) : item(b)[sortString].localeCompare(item(a)[sortString])
+      sortedList = [...lists[selectedList]].sort( (a, b) => {
+        return ascending ? item(a)[sortString].localeCompare(item(b)[sortString]) : item(b)[sortString].localeCompare(item(a)[sortString])
       });
-      break;
-  //   default:
-  //     sortedList = [...state.lists[state.selectedList]].sort( (a,b) => {
-  //       return asc ? 
-  //         (state.all[a])[sortString] - (state.all[b])[sortString] : (state.all[b])[sortString] - (state.all[a])[sortString];
-  //       });
-  //     break;
-  }
-  return sortedList.map(id => state.all[id]).filter(spell => spell.name.toLowerCase().includes(state.filter));     
-}
+        break;
+      //   default:
+      //     sortedList = [...state.lists[state.selectedList]].sort( (a,b) => {
+      //       return asc ? 
+      //         (state.all[a])[sortString] - (state.all[b])[sortString] : (state.all[b])[sortString] - (state.all[a])[sortString];
+      //       });
+      //     break;
+      }
+      return sortedList.map(id => allSpells[id]).filter(spell => spell.name.toLowerCase().includes(filter));     
+    }
+);
+
+// export const getSortedSpells = (state) => {
+//   const item = id => state.all[id];
+//   let sortString = state.sort;
+//   let asc = state.ascending;
+//   let sortedList;
+//   switch (state.sort){
+//       //    case 'name':
+//       default:
+//       sortedList = [...state.lists[state.selectedList]].sort( (a, b) => {
+//         return asc ? item(a)[sortString].localeCompare(item(b)[sortString]) : item(b)[sortString].localeCompare(item(a)[sortString])
+//       });
+//       break;
+//   //   default:
+//   //     sortedList = [...state.lists[state.selectedList]].sort( (a,b) => {
+//   //       return asc ? 
+//   //         (state.all[a])[sortString] - (state.all[b])[sortString] : (state.all[b])[sortString] - (state.all[a])[sortString];
+//   //       });
+//   //     break;
+//   }
+//   return sortedList.map(id => state.all[id]).filter(spell => spell.name.toLowerCase().includes(state.filter));     
+// }
